4ce2d3bb124838d6d6ba729919ac7bd8e7e650c9
Tanagra Andria <design.andria@gmail.com>
2021-03-17 12:19:09 -0300

    create routers

--- server/routes/ExampleRouter.js
+++ server/routes/ExampleRouter.js
@@ -1,9 +1,15 @@
 const { Router } = require('express')
 const ExampleController = require('../controller/ExampleController')
+const KitchenController = require('../controller/KitchenController')
+const WaiterController = require('../controller/WaiterController')
 
 const router = Router()
 
 // aqui vai as requisições
 router.get("/", ExampleController.getAllExamples)
+router.get("/", KitchenController.getAllNewOrder)
+router.get("/", WaiterController.getAllNewOrder)
+
+
 
 module.exports = router
