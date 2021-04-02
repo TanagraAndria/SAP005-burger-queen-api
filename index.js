afdd7515e4e1599d737eb3a065f6cb8be879e0e1
Tanagra Andria <design.andria@gmail.com>
2021-04-02 18:04:04 -0300

    ajuste na porta

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const routes = require('./server/routes/index')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
+if(process.env.NODE_ENV !== 'production') {
+  dotenv.config();
+}
 app.use(express.json()); 
-app.use('/', routes);
+app.use(routes);
 
-app.get('*', (req, res) => {
-  res.send('Chef Api')
-})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
